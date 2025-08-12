3c518404b057376bbd8fd345953c365171af3cca
PC-2022-010 <daffarafdhan@gmail.com>
2025-08-12 11:54:37 +0700

    benerin navbar

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,12 +8,6 @@
     <!-- Import Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Anton&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <div class="py-6 sm:py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 animate-fade-in">
-                    <p class="mb-5">You are an Admin</p>
-                    
                     <section class="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5 rounded-lg">
                         <div class="mx-auto max-w-screen-xl px-4 lg:px-12">
                             
@@ -30,25 +24,25 @@
 
                             <!-- Statistik Cards -->
                             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 mt-6 sm:rounded-lg">
-                                <div class="stat-card bg-gradient-to-br from-gray-800 to-gray-900 text-white">
+                                <div class="stat-card bg-gradient-to-br from-gray-800 to-gray-900 text-white size-56 rounded-xl p-5">
                                     <p class="text-sm opacity-75">Total Siswa</p>
-                                    <h2 class="number">308</h2>
+                                    <h2 class="number ">308</h2>
                                 </div>
-                                <div class="stat-card bg-gradient-to-br from-blue-500 to-blue-700 text-white">
+                                <div class="stat-card bg-gradient-to-br from-blue-500 to-blue-700 text-white size-56 rounded-xl p-5">
                                     <p class="text-sm opacity-75">Total Hadir Hari Ini</p>
-                                    <h2 class="number">240</h2>
+                                    <h2 class="number ">240</h2>
                                 </div>
-                                <div class="stat-card bg-gradient-to-br from-green-500 to-green-700 text-white">
+                                <div class="stat-card bg-gradient-to-br from-green-500 to-green-700 text-white size-56 rounded-xl p-5">
                                     <p class="text-sm opacity-75">Izin</p>
-                                    <h2 class="number">2</h2>
+                                    <h2 class="number ">2</h2>
                                 </div>
-                                <div class="stat-card bg-gradient-to-br from-red-500 to-red-700 text-white">
+                                <div class="stat-card bg-gradient-to-br from-red-500 to-red-700 text-white size-56 rounded-xl p-5">
                                     <p class="text-sm opacity-75">Sakit</p>
-                                    <h2 class="number">11</h2>
+                                    <h2 class="number ">11</h2>
                                 </div>
-                                <div class="stat-card bg-gradient-to-br from-black to-gray-800 text-white">
+                                <div class="stat-card bg-gradient-to-br from-black to-gray-800 text-white size-56 rounded-xl p-5">
                                     <p class="text-sm opacity-75">Tanpa Keterangan</p>
-                                    <h2 class="number">0</h2>
+                                    <h2 class="number ">0</h2>
                                 </div>
                             </div>
 
