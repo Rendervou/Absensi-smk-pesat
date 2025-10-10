5aca79960508fe494895c5897b678ef64fdf8d81
SOoyaaqt12 <daffa.rafif364@smk.belajar.id>
2025-10-10 17:28:40 +0700

    benerin app di bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Role;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,13 +12,20 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function ($middleware) {
-        // Tambahkan middleware TrustProxies
-        $middleware->trustProxies(at: '*', headers:              Request::HEADER_X_FORWARDED_FOR
+    ->withMiddleware(function (Middleware $middleware) {
+        // Trust proxies untuk Railway/production
+        $middleware->trustProxies(at: '*', headers: 
+            Request::HEADER_X_FORWARDED_FOR
             | Request::HEADER_X_FORWARDED_HOST
             | Request::HEADER_X_FORWARDED_PORT
             | Request::HEADER_X_FORWARDED_PROTO
-            | Request::HEADER_X_FORWARDED_AWS_ELB);
+            | Request::HEADER_X_FORWARDED_AWS_ELB
+        );
+        
+        // Daftarkan middleware alias
+        $middleware->alias([
+            'role' => Role::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
